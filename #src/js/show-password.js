413850e877006a013b14604a0be05f608dbbd403
Serj-L <svlepniakov@gmail.com>
2021-04-12 12:26:25 +0300

    edit password show/hide function

--- #src/js/show-password.js
+++ #src/js/show-password.js
@@ -1,36 +1,33 @@
  //show hidden password
- function show_hide_password(target){
-    var input = document.getElementById('password-input');
-    if (input.getAttribute('type') == 'password') {
-        target.classList.add('view');
-        input.setAttribute('type', 'text');
-    } else {
-        target.classList.remove('view');
-        input.setAttribute('type', 'password');
-    }
-    return false;
-}
+ const controlButtons = document.querySelectorAll('.modal-password-show-icon');
 
-function show_hide_password_signUp(target){
-    var input = document.getElementById('sign-up-password-input');
-    if (input.getAttribute('type') == 'password') {
-        target.classList.add('view');
-        input.setAttribute('type', 'text');
-    } else {
-        target.classList.remove('view');
-        input.setAttribute('type', 'password');
-    }
-    return false;
-}
+ if (controlButtons.length > 0) {
+    controlButtons.forEach(controlButton => {
+        controlButton.addEventListener("click", (e) => {
 
-function show_hide_confirm_password_signUp(target){
-    var input = document.getElementById('sign-up-confirm-password-input');
-    if (input.getAttribute('type') == 'password') {
-        target.classList.add('view');
-        input.setAttribute('type', 'text');
-    } else {
-        target.classList.remove('view');
-        input.setAttribute('type', 'password');
-    }
-    return false;
+            const controlButtonId = e.target.id;
+
+            const input = getInputClass (controlButtonId);
+            function getInputClass (controlButtonId) {
+                if (controlButtonId === 'control-btn-sign-in') {
+                    return document.querySelector('.modal-sign-in__password-input')}
+                if (controlButtonId === 'control-btn-sign-up') {
+                    return document.querySelector('.modal-sign-up__password-input')}
+                if (controlButtonId === 'control-btn-sign-up-confirm') {
+                    return document.querySelector('.modal-sign-up__confirm-password-input')}
+            };
+
+            function show_hide_password () {
+                if (input.getAttribute('type') === 'password') {
+                    document.getElementById(controlButtonId).classList.add('view');
+                    input.setAttribute('type', 'text');
+                } else {
+                    document.getElementById(controlButtonId).classList.remove('view');
+                    input.setAttribute('type', 'password');
+                }
+                return false;
+            }
+            show_hide_password ()
+        });
+    });
 }
